a9636c6a3be1cc3564614072ce3f10ea69669f84
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-02-13 22:03:40 -0500

    nope, snaps have to match -- always mask with the same number

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -3,6 +3,6 @@ scrub_environment_code <- function(x) {
   # linux: 12
   # windows: 16
   m <- regexpr("(?<=<environment: 0x)[0-9a-f]+(?=>)", x, perl = TRUE)
-  regmatches(x, m) <- strrep("0", max(0, attr(m, "match.length")))
+  regmatches(x, m) <- strrep("0", max(0, 12L))
   x
 }
