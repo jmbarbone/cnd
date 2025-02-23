a6d4ee834c3a77a4f8b136f396ecf4f06094425f
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-02-22 20:45:57 -0500

    adds one more test for cep

--- tests/testthat/test-cep.R
+++ tests/testthat/test-cep.R
@@ -6,3 +6,24 @@ test_that("test_cep()", {
   # retain here to get the registry
   test_documentation("cep")
 })
+
+test_that("cep in use", {
+  skip_on_cran()
+  skip_if_not_installed("here")
+  err <- tempfile()
+  on.exit(file.remove(err))
+
+  expect_no_error({
+    system2(
+      "Rscript",
+      c("--vanilla", here::here("tools/test-cep-in-use.R")),
+      stdout = FALSE,
+      stderr = err
+    )
+
+    warn <- readLines(err)
+    if (length(warn) > 0) {
+      stop(paste(warn, collapse = "\n"))
+    }
+  })
+})
---
+++ tools/test-cep-in-use.R
@@ -0,0 +1,26 @@
+library(testthat, warn.conflicts = FALSE)
+library(cnd)
+library(cep)
+
+test_that("registry found in package", {
+  expect_s3_class(
+    cep:::.__CND_REGISTRY__.,
+    "cnd:registry"
+  )
+})
+
+test_that("registrary is picking up the conditions from the cep package", {
+  from_cnd <- conditions("cep")
+  from_cep <- Filter(
+    \(e) inherits(e, "cnd::condition_generator"),
+    as.list(getNamespace("cep"), all.names = TRUE, sorted = TRUE)
+  )
+  expect_setequal(from_cnd, from_cep)
+})
+
+test_that("cep registry is loaded into cnd", {
+  expect_identical(
+    cnd:::registrar$.__REGISTRIES__.$cep,
+    cep:::.__CND_REGISTRY__.
+  )
+})
