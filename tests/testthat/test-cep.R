e8ea3abb0773e56886f678dfb212ad83e6a6e942
Jordan Mark Barbone <jmbarbone@gmail.com>
2025-02-23 01:27:13 -0500

    cmd check first

--- tests/testthat/test-cep.R
+++ tests/testthat/test-cep.R
@@ -1,3 +1,8 @@
+test_that("cep R CMD check", {
+  skip_on_cran()
+  check_rcmdcheck()
+})
+
 test_that("test_cep()", {
   on.exit(registrar$remove("cep"))
   skip_on_cran()
@@ -5,9 +10,4 @@ test_that("test_cep()", {
 
   # retain here to get the registry
   test_documentation("cep")
-})
-
-test_that("cep R CMD check", {
-  skip_on_cran()
-  check_rcmdcheck()
 })
